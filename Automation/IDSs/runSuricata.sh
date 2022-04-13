31e12d79ca0ea264bc0682b27904aad714390c8c
baadam3 <63235454+baadam3@users.noreply.github.com>
2022-04-13 13:04:43 +0300

    Contains the template to run Suricata measurements

---
+++ Automation/IDSs/runSuricata.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/path/to/GNU/time -o /path/to/output/file -a -f '<format_of_output_log>' /path/to/suricata -c /path/to/config -s /path/to/rules -i <NIC_interface> >> /path/to/suricata/output/file
